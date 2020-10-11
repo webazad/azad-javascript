4a66b20f50fddb39893f2d1c6b9f1163c24cfbc6
Azad <azad@azad.com>
2020-10-11 09:23:06 +0600

    Get

---
+++ azad-vuejs/azad-vuejs-3/src/components/Footer.vue
@@ -0,0 +1,62 @@
+<template>
+  <footer class="azad-footer">
+    <div class="footer-top">
+      <div class="">
+      <h1>Logo</h1>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    <div class="">
+      <h1>Logo</h1>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    <div class="">
+      <h1>Logo</h1>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    <div class="">
+      <h1>Logo</h1>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    </div>
+    <div class="copyright">
+      <p>All Rights Reserved By Azad 2020</p>
+    </div>
+  </footer>
+</template>
+
+<style>
+footer {
+  margin-top: auto;  
+}
+.footer-top {
+  background: #e3e3e3;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 1rem;
+  padding: 2rem 0;
+}
+.copyright {
+  background: black;
+  padding: 2rem 0;  
+}
+</style>
---
+++ azad-vuejs/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="hello">
+    <div class="">
+      <h1>Logo Text</h1>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  padding: 2rem 0;
+  background: blue;
+}
+ul li a{
+  color: white;
+}
+</style>
